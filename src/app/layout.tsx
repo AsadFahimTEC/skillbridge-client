a756595d07fa5c5063a7da1f92be02618b55e5c6
AsadFahimTEC <fah485434@gmail.com>
2026-01-27 09:04:34 +0600

    footer added properly

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Navbar1 } from "@/components/navbar1";
+import { Footer2 } from "@/components/footer2";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,7 +31,10 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Navbar1></Navbar1>
+        
         {children}
+
+        <Footer2></Footer2>
       </body>
     </html>
   );
