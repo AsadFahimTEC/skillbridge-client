ef2375b811423a621cb5981cfba9177d78eb0f64
AsadFahimTEC <fah485434@gmail.com>
2026-01-30 12:38:32 +0600

    tutor service all issue solve properly

--- src/app/tutors/[id]/page.tsx
+++ src/app/tutors/[id]/page.tsx
@@ -1,6 +1,7 @@
 import TutorProfileFrom from "@/app/components/TutorProfileFrom";
 
 export default function TutorsPageDynamicPage() {
+  
   return (
     <div className="p-6">
       <h1 className="text-xl font-bold mb-4">Edit Tutor Profile</h1>
