d2c7b8a4d78851204060fc6fdf7a1be20f8a301a
AsadFahimTEC <fah485434@gmail.com>
2026-01-30 18:42:06 +0600

    all routes setup finish

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
+import { FeaturedTutors } from "@/components/feature17";
 import { Button } from "@/components/ui/button";
 
 
 export default function HomePage() {
   return (
     <div>
-      <Button variant={"outline"}>Click Here</Button>
+      {/* <Button variant={"outline"}>Click Here</Button> */}
+      <FeaturedTutors></FeaturedTutors>
     </div>
   );
 }
---
+++ src/constants/routes.ts
@@ -0,0 +1,10 @@
+export const ROUTES = {
+    HOME: "/",
+    TUTORS: "/tutors",
+    LOGIN: "/login",
+    REGISTER: "/register",
+
+    STUDENT_DASHBOARD: "/bookings",
+    TUTOR_DASHBOARD: "/tutors",
+    ADMIN_DASHBOARD: "/admin"
+}
