52929dbce6ff7af5d752ac683f7e56d48b7aea2f
AsadFahimTEC <fah485434@gmail.com>
2026-01-30 09:52:16 +0600

    login and register page design finish properly

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,8 +1,11 @@
+import { LoginForm } from "@/components/login-form";
 
 export default function LoginPage() {
   return (
-    <div>
-      This is login page
+    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+      <div className="w-full max-w-sm">
+        <LoginForm></LoginForm>
+      </div>
     </div>
   );
 }
